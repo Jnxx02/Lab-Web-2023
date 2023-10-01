8e6e36a6a8bbbf055066d087bfc504578dd59442
Jonathan Kwan <jonathankwan056@gmail.com>
2023-10-01 14:30:04 +0800

    feat: materi dan tugas pertemuan 4

---
+++ Pertemuan 4 - Javascript/script.js
@@ -0,0 +1,99 @@
+// var i = [1, 2, "Hello World", [5, 6, "Hello Wolf"]]
+// console.log(i[3][1]);
+
+// // Object
+// var obj = {
+//     "nama": "Oi",
+//     "NIM": "oi"
+// }
+
+// console.log(obj["nama"] + " " + obj.NIM);
+
+// // Conditional
+// let x = [5, 10, 15, 20]
+
+// if(x[1] % 3 == 2) {
+//     console.log(" If Benar");
+// } else if (x[1] % 3 == 1){
+//     console.log("Else If Benar");
+// } else {
+//     console.log("kedua kondisi tidak benar");
+// }
+
+// // Looping
+// i.forEach(x => {
+//     console.log(x);
+// });
+
+// for (let i = 0; i < x.length; i++) {
+//     console.log(i);
+
+// }
+
+// // Function
+// function tambah(nilai1, nilai2) {
+//     return nilai1 + nilai2
+// }
+// console.log(tambah(5, 8));
+
+// Switch Case
+// var input = parseInt(prompt("Masukkan Nilai: "))
+
+// switch (input) {
+//     case 1:
+//         console.log("Start");
+//         break;
+//     case 2:
+//         console.log("Continue");
+//         break;
+//     case 3:
+//         console.log("Option");
+//         break;
+//     case 4:
+//         console.log("Exit");
+//         break;
+//     default:
+//         console.log("Unexpected input");
+//         break;
+// }
+
+// Latihan
+// 1
+// var input = parseInt(prompt("Masukkan angka"))
+
+// if (input % 2 == 0) {
+//     console.log("Genap");
+// } else if (input % 2 == 1){
+//     console.log("Ganjil");
+// } else {
+//     alert("Masukkan angka")
+// }
+
+// 2 
+// var nama = prompt()
+// var nilai = parseInt(prompt())
+// var hasil = ""
+
+// if(nilai > 90) {
+//     var hasil = "Cumlaude"
+//     console.log("Selamat " + nama + " adalah mahasiswa Cumlaude dengan nilai " + nilai);
+// } else if(nilai > 50) {
+//     var hasil = "Lulus"
+//     console.log("Selamat " + nama + " berhasil lulus dengan nilai " + nilai);
+// } else if (nilai > 0) {
+//     var hasil = "Remedial"
+//     console.log("Mohon maaf " + nama + " harus remedial tahun depan karena nilai anda " + nilai);
+// } else {
+//     console.log("Masukkan inputan dengan benar");
+// }
+
+// 3 - Fibonacci
+// let a = prompt()
+// let b = 0
+// let c = 1
+// for (let i = 0; i <= a; i++) {
+//     console.log((b));
+//     let d = b + c
+//     b = c
+//     c = d
+// }
