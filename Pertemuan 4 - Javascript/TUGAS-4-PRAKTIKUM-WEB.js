3a557d2e328a7be79d12de559c2a9e049dc164d3
Jonathan Kwan <jonathankwan056@gmail.com>
2023-10-02 20:20:55 +0800

    fix: memperbaiki beberapa variabel

--- Pertemuan 4 - Javascript/TUGAS-4-PRAKTIKUM-WEB.js
+++ Pertemuan 4 - Javascript/TUGAS-4-PRAKTIKUM-WEB.js
@@ -1,14 +1,14 @@
 // 1. Eksponensial
 function Eksponen(angka, pangkat) {
-    if (pangkat == 0) {
-        return 1;
-    } else {
-        let hasil = angka;
-        for (let i = 1; i < pangkat; i++) {
-            hasil *=angka;
-        }
-        return hasil;
+  if (pangkat == 0) {
+    return hasil = 1;
+  } else {
+    let hasil = angka;
+    for (let i = 1; i < pangkat; i++) {
+      hasil *= angka;
     }
+    return hasil;
+  }
 }
 
 console.log(Eksponen(2, 10));
@@ -16,80 +16,75 @@ console.log(Eksponen(3, 4));
 
 // 2. Cipher Text
 function CipherText(text, n) {
-    let result = "";
-  
-    for (let i = 0; i < text.length; i++) {
-      let char = text[i];
+  let result = "";
 
-      if (/[a-zA-Z]/.test(char)) {
-        let isUpperCase = char === char.toUpperCase();
-        let offset = isUpperCase ? 65 : 97; 
-        let shiftedChar = String.fromCharCode(((char.charCodeAt(0) - offset + n) % 26) + offset);
-        result += shiftedChar;
-      } else {
-        result += char; 
+  for (let i = 0; i < text.length; i++) {
+    let char = text[i];
+    if (/[a-zA-Z]/.test(char)) {
+      let isUpperCase = char === char.toUpperCase();
+      let offset = isUpperCase ? 65 : 97;
+      let shiftedChar = String.fromCharCode(((char.charCodeAt(0) - offset + n) % 26) + offset);
+      result += shiftedChar;
+    } else {
+      result += char;
+    }
+  }
+  return result;
+}
+
+console.log(CipherText("abc", 1));
+console.log(CipherText("Indonesia", 13));
+
+// 3. Kata Palindrom
+function isPalindrome(word) {
+  word = word.toLowerCase().replace(/\s/g, '');
+  const reversedWord = word.split('').reverse().join('');
+  return word === reversedWord;
+}
+
+console.log(isPalindrome("malam"));
+console.log(isPalindrome("apa"));
+console.log(isPalindrome("131"));
+console.log(isPalindrome("12321"));
+console.log(isPalindrome("Kodok"));
+console.log(isPalindrome("kasur ini rusak"));
+
+// 4. Mengurutkan angka dari yang terkecil
+function selectionSort(arr) {
+  const n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    let index = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[index]) {
+        index = j;
       }
     }
-    return result;
+    if (index !== i) {
+      const temp = arr[i];
+      arr[i] = arr[index];
+      arr[index] = temp;
+    }
   }
- 
-  console.log(CipherText("abc", 1));
-  console.log(CipherText("Indonesia", 13)); 
+}
 
-  // 3. Kata Palindrom
-  function isPalindrome(word) {
-    word = word.toLowerCase().replace(/\s/g, '');
-    const reversedWord = word.split('').reverse().join('');
-    return word === reversedWord;
-  }
-  
-  console.log(isPalindrome("malam"));
-  console.log(isPalindrome("apa"));
-  console.log(isPalindrome("131"));
-  console.log(isPalindrome("12321"));
-  console.log(isPalindrome("Kodok"));
-  console.log(isPalindrome("kasur ini rusak"));
-  
-  // 4. Mengurutkan angka dari yang terkecil
-  function selectionSort(arr) {
-    const n = arr.length;
-  
-    for (let i = 0; i < n - 1; i++) {
-      let index = i;
-      for (let j = i + 1; j < n; j++) {
-        if (arr[j] < arr[index]) {
-          index = j;
-        }
-      }
-      if (index !== i) {
-        const temp = arr[i];
-        arr[i] = arr[index];
-        arr[index] = temp;
-      }
-    }
-  }
-  
-  const inputArr = [50, 20, 1, 45, 3];
-  selectionSort(inputArr);
-  console.log(inputArr);
+const inputArr = [50, 20, 1, 45, 3];
+console.log(selectionSort(inputArr));
 
-  // 5. Mengubah angka ke binary
-  function numberToBinary(number) {
-    if (number === 0) {
-      return "0";
-    }
-  
-    let binary = "";
-    while (number > 0) {
-      let remainder = number % 2;
-      binary = remainder + binary;
-      number = Math.floor(number / 2);
-    }
-    return binary;
+// 5. Mengubah angka ke binary
+function numberToBinary(number) {
+  if (number === 0) {
+    return "0";
   }
-  
-  const angkaDesimal = 14;
-  const angkaBiner = numberToBinary(angkaDesimal);
-  console.log(angkaBiner);
-  
-  
+
+  let binary = "";
+  while (number > 0) {
+    let check = number % 2;
+    binary = check + binary;
+    number = Math.floor(number / 2);
+  }
+  return binary;
+}
+
+console.log(numberToBinary(14));
+
