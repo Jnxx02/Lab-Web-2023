568214a3b864e1767f51f7273cf9d29bdbe2b4c6
Jonathan Kwan <jonathankwan056@gmail.com>
2023-10-03 15:20:36 +0800

    fix: memperbaiki kode

--- Pertemuan 4 - Javascript/TUGAS-4-PRAKTIKUM-WEB.js
+++ Pertemuan 4 - Javascript/TUGAS-4-PRAKTIKUM-WEB.js
@@ -1,75 +1,78 @@
-// 1. Eksponensial
-function Eksponen(angka, pangkat) {
-  if (pangkat == 0) {
-    return hasil = 1;
-  } else {
-    let hasil = angka;
-    for (let i = 1; i < pangkat; i++) {
-      hasil *= angka;
-    }
-    return hasil;
-  }
-}
+// // 1. Eksponensial
+// function eksponen(angka, pangkat) {
+//   if (pangkat == 0) {
+//     return hasil = 1;
+//   } else {
+//     let hasil = angka;
+//     for (let i = 1; i < pangkat; i++) {
+//       hasil *= angka;
+//     }
+//     return hasil;
+//   }
+// }
 
-console.log(Eksponen(2, 10));
-console.log(Eksponen(3, 4));
+// console.log(eksponen(2, 10));
+// console.log(eksponen(3, 4));
 
-// 2. Cipher Text
-function CipherText(text, n) {
-  let result = "";
+// // 2. Chipper Text
+// function chipperText(text, n) {
+//   let result = "";
 
-  for (let i = 0; i < text.length; i++) {
-    let char = text[i];
-    if (/[a-zA-Z]/.test(char)) {
-      let isUpperCase = char === char.toUpperCase();
-      let offset = isUpperCase ? 65 : 97;
-      let shiftedChar = String.fromCharCode(((char.charCodeAt(0) - offset + n) % 26) + offset);
-      result += shiftedChar;
-    } else {
-      result += char;
-    }
-  }
-  return result;
-}
+//   for (let i = 0; i < text.length; i++) {
+//     let char = text[i];
 
-console.log(CipherText("abc", 1));
-console.log(CipherText("Indonesia", 13));
+//     if (/[a-zA-Z]/.test(char)) {
+//       let isUpperCase = char === char.toUpperCase();
+//       let offset = isUpperCase ? 65 : 97;
+//       let shiftedChar = String.fromCharCode(((char.charCodeAt(0) - offset + n) % 26) + offset);
+//       result += shiftedChar;
+//     } else {
+//       result += char;
+//     }
+//   }
+//   return result;
+// }
 
-// 3. Kata Palindrom
-function isPalindrome(word) {
-  word = word.toLowerCase().replace(/\s/g, '');
-  const reversedWord = word.split('').reverse().join('');
-  return word === reversedWord;
-}
+// console.log(chipperText("abc", 1));
+// console.log(chipperText("Indonesia", 13));
 
-console.log(isPalindrome("malam"));
-console.log(isPalindrome("apa"));
-console.log(isPalindrome("131"));
-console.log(isPalindrome("12321"));
-console.log(isPalindrome("Kodok"));
-console.log(isPalindrome("kasur ini rusak"));
+// // 3. Kata Palindrom
+// function isPalindrome(word) {
+//   word = word.toLowerCase().replace(/\s/g, '');
+//   const reversedWord = word.split('').reverse().join('');
+//   return word === reversedWord;
+// }
 
-// 4. Mengurutkan angka dari yang terkecil
-function selectionSort(arr) {
-  const n = arr.length;
+// console.log(isPalindrome("malam"));
+// console.log(isPalindrome("apa"));
+// console.log(isPalindrome("131"));
+// console.log(isPalindrome("12321"));
+// console.log(isPalindrome("Kodok"));
+// console.log(isPalindrome("kasur ini rusak"));
+// console.log(isPalindrome("siapa"));
 
-  for (let i = 0; i < n - 1; i++) {
-    let index = i;
-    for (let j = i + 1; j < n; j++) {
-      if (arr[j] < arr[index]) {
-        index = j;
-      }
-    }
-    if (index !== i) {
-      const temp = arr[i];
-      arr[i] = arr[index];
-      arr[index] = temp;
-    }
-  }
-}
+// // 4. Mengurutkan angka dari yang terkecil
+// function sortArray(arr) {
+//   const n = arr.length;
 
-const inputArr = [50, 20, 1, 45, 3];
-console.log(selectionSort(inputArr));
+//   for (let i = 0; i < n - 1; i++) {
+//     let index = i;
+//     for (let j = i + 1; j < n; j++) {
+//       if (arr[j] < arr[index]) {
+//         index = j;
+//       }
+//     }
+//     if (index !== i) {
+//       const temp = arr[i];
+//       arr[i] = arr[index];
+//       arr[index] = temp;
+//     }
+//   }
+//   return arr;
+// }
+
+// const inputArr = [50, 20, 1, 45, 3];
+// console.log(sortArray(inputArr));
 
 // 5. Mengubah angka ke binary
 function numberToBinary(number) {
